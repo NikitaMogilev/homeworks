54183b88ea0e8cdcda0e3f811378e5fabf034ca7
Nikita Mogilev <nikitamogilev0@gmail.com>
2022-11-29 23:30:35 +0300

    Complete task_3_4

--- homework3/task_3_4.py
+++ homework3/task_3_4.py
@@ -1,21 +1,14 @@
-from itertools import chain
-
-
-def mergeDict(dict1, dict2):
-    for k, v in chain(dict1.items(), dict2.items()):
-        if dict1.get(k):
-            dict1[k] = [v, v]
-        else:
-            dict1[k] = [None, v]
-    return dict1
-
-
 dict1 = {'a': 1, 'b': 2, 'c': 3}
 dict2 = {'c': 3, 'd': 4, 'e': 5}
 
-print(dict1)
-print(dict2)
+dict3 = {}
 
-dict3 = mergeDict(dict1, dict2)
-
-print(dict3)
+for key, value in dict1.items():
+    if dict1.get(key) != dict2.get(key):
+        dict3[key] = [value, None]
+    else:
+        dict3[key] = [value, value]
+for key, value in dict2.items():
+    if dict1.get(key) != dict2.get(key):
+        dict3[key] = [None, value]
+print(f"dict3 = {dict3}")
