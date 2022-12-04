dad205d998d3bbbf1edad60e2adc151fd6eebd0c
Nikita Mogilev <nikitamogilev0@gmail.com>
2022-12-04 11:15:15 +0300

    Complete task_5_2

--- homework5/homework_5_2.py
+++ homework5/homework_5_2.py
@@ -12,11 +12,9 @@ while True:
     if user_index == index:
         print(f'число по заданному индексу {index} = {next_number}')
         break
-    
     if user_index == 1:
         print('число по заданному индексу 1 = 1')
         break
-    
     if user_index == 0:
         print('число по заданному индексу 0 = 0')
         break
